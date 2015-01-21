1e1629ec0636781ea405421933afce0eb60758ba
jjvanraffe <jjvanraffe@live.com>
2015-01-21 12:10:06 +0100

    Board tests done
    All board tests implemented some documentation is still missing and will
    be added soon.

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -1,19 +1,18 @@
 package tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.*;
-import model.Mark;
 import model.*;
-;
+
 public class BoardTest {
     private Board board;
     
+    // Sets the board to an empty board.
 	@Before
 	public void setUp() throws Exception {
 		board = new Board();
 	}
-
+	// Tests whether the board is really empty.
 	@Test
 	public void testInitialState() {
 		for(int i = 0; i < Board.WIDTH * Board.HEIGHT; i++){
@@ -21,6 +20,7 @@ public class BoardTest {
 		}
 	}
 	
+	// Tests whether the deepcopy really gives a copy of the board as it is at that time.
 	@Test
 	public void testDeepcopy(){
 		Board board1 = board.deepCopy();
@@ -29,6 +29,7 @@ public class BoardTest {
 		}
 	}
 	
+	//Tests whether the index gives the right index when the row and column are given.
 	@Test
 	public void testIndex(){
 		int a = 1;
@@ -42,6 +43,7 @@ public class BoardTest {
 		assertEquals(board.index(a,b), 800);
 	}
 	
+	// Tests whether really only the fields that are meant to be checked are selected. 
 	@Test
 	public void testIsField1(){
 		for(int i = 0; i < Board.WIDTH * Board.HEIGHT; i++){
@@ -55,7 +57,7 @@ public class BoardTest {
 		}
 	}
 	
-	//Mist nog 1 branch
+	//Tests whether only the columns and rows which are meant to be in the board are allowed.
 	@Test
 	public void testIsField2(){
 		for(int i = 0; i < Board.WIDTH; i++){
@@ -104,6 +106,7 @@ public class BoardTest {
 		}
 	}
 	
+	// Tests whether the right thing in the right field is given.
 	@Test
 	public void testGetField1(){
 		board.setField(1, Mark.BLU);
@@ -114,6 +117,7 @@ public class BoardTest {
 		
 	}
 	
+	// Tests whether the right thing in the right field is given.
 	@Test
 	public void testGetField2(){
 		board.setField(5, 1, Mark.BLU);
@@ -123,6 +127,7 @@ public class BoardTest {
 		assertEquals(board.getField(5, 2), Mark.RED);
 	}
 	
+	// Tests whether the querry gives the right boolean when a field is selected.
 	@Test
 	public void testIsEmptyField1(){
 		board.setField(1, Mark.BLU);
@@ -132,6 +137,7 @@ public class BoardTest {
 		assertFalse(board.isEmptyField(2));
 	}
 	
+	// Tests whether the querry gives the right boolean when a field is selected.
 	@Test
 	public void testIsEmptyField2(){
 		board.setField(5, 1, Mark.BLU);
@@ -141,6 +147,7 @@ public class BoardTest {
 		assertFalse(board.isEmptyField(5, 2));
 	}
 	
+	// Tests whether it allows the right colums.
 	@Test
 	public void testIsColumn(){
 		for(int i = 0; i < Board.WIDTH; i++){
@@ -154,6 +161,7 @@ public class BoardTest {
 		}
 	}
 	
+	// Tests whether a column still has an empty field.
 	@Test
 	public void testContainsEmptyColumn(){
 		board.setField(5, 1, Mark.BLU);
@@ -173,7 +181,7 @@ public class BoardTest {
 		assertFalse(board.containsEmptyColumn(2));
 	}
 	
-	//1 branch missed.
+	// Tests whether the right field is selected by the querry.
 	@Test
 	public void testDetermineField() {
 		board.setField(5, 1, Mark.BLU);
@@ -187,6 +195,7 @@ public class BoardTest {
 		assertEquals(board.determineField(2), board.index(0, 2));
 	}
 	
+	// Tests whether the querry gives the correct boolean when the board is full and when the board is not full.
 	@Test
 	public void testIsFull() {
 		assertFalse(board.isFull());
@@ -213,7 +222,7 @@ public class BoardTest {
 		assertTrue(board.isFull());
 	}
 	
-	//nog 1 branch gemist.
+	// Tests whether 
 	@Test
 	public void testHasRow() {
 		assertFalse(board.hasRow(Mark.RED));
@@ -319,6 +328,7 @@ public class BoardTest {
 				board.setField(i, Mark.XXX);
 			}
 		}
+		assertTrue(board.gameOver());
 		for(int i = 0; i < Board.WIDTH; i++){
 			for(int j = 0; j < Board.HEIGHT; j++){
 				if(i % 2 == 0){
@@ -342,5 +352,139 @@ public class BoardTest {
 		assertTrue(board.gameOver());
 	}
 	
+	@Test
+	public void testIsWinner() {
+		assertFalse(board.isWinner(Mark.BLU));
+		assertFalse(board.isWinner(Mark.RED));
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i < 4){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i > 6 && i < 11){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.isWinner(Mark.BLU));
+		assertTrue(board.isWinner(Mark.RED));
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i == 0|| i == 7 || i == 14 || i == 21){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i == 1 || i == 8 || i == 15 || i == 22){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.isWinner(Mark.BLU));
+		assertTrue(board.isWinner(Mark.RED));
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i == 0|| i == 8 || i == 16 || i == 24){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i == 13 || i == 19 || i == 25 || i == 31){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.isWinner(Mark.BLU));
+		assertTrue(board.isWinner(Mark.RED));
+	}
 	
+	// nog 1 branch gemist. Dit is waarschijnlijk de branch die true geeft wanneer ze beide waar zijn maar daar hoeft niet op getest te worden.
+	@Test
+	public void testHasWinner() {
+		assertFalse(board.hasWinner());
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i < 4){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i > 6 && i < 11){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.hasWinner());
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i == 0|| i == 7 || i == 14 || i == 21){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i == 1 || i == 8 || i == 15 || i == 22){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.hasWinner());
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			if(i == 0|| i == 8 || i == 16 || i == 24){
+				board.setField(i, Mark.BLU);
+			}
+			else if(i == 13 || i == 19 || i == 25 || i == 31){
+				board.setField(i, Mark.RED);
+			}
+			else{
+				board.setField(i, Mark.XXX);
+			}
+		}
+		assertTrue(board.hasWinner());
+	}
+	
+	@Test
+	public void testToString() {
+		String method = board.toString();
+		String[] numbering = {"  0  |  1  |  2  |  3  |  4  |  5  |  6  ",
+		    	"-----+-----+-----+-----+-----+-----+-----"};
+		String line = numbering[1];
+        String s = "";
+        for (int i = 0; i < Board.HEIGHT; i++) {
+            String row = "";
+            for (int j = 0; j < Board.WIDTH; j++) {
+                row = row + " " + board.getField(i, j).toString() + " ";
+                if (j < Board.WIDTH - 1) {
+                    row = row + "|";
+                }
+            }
+            s = s + row;
+            if (i < Board.HEIGHT - 1) {
+                s = s + "\n" + line + "\n";
+            }
+        }
+        s = s + "\n" + line + "\n" + numbering[0];
+        assertEquals(method, s);
+	}
+	
+	@Test
+	public void testReset() {
+		board.setField(8, Mark.RED);
+		board.setField(5, Mark.RED);
+		board.setField(21, Mark.RED);
+		board.setField(32, Mark.RED);
+		board.setField(23, Mark.RED);
+		board.reset();
+		for(int i = 0; i < Board.HEIGHT * Board.WIDTH; i++){
+			assertEquals(board.getField(i), Mark.XXX);
+		}
+	}
+	
+	@Test
+	public void testSetField1(){
+		board.setField(1, Mark.BLU);
+		assertEquals(board.getField(1), Mark.BLU);
+	}
+	
+	@Test
+	public void testSetField2() {
+		board.setField(1, 2, Mark.BLU);
+		assertEquals(board.getField(1, 2), Mark.BLU);
+	}
 }
