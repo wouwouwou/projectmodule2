114e21d3328928e3b956991e931188a4fe15b226
Wouter Bos <wouter_bos96@hotmail.com>
2015-01-21 23:06:50 +0100

    Work on invitesupport from Client

--- src/controller/Client.java
+++ src/controller/Client.java
@@ -7,6 +7,9 @@ import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.*;
+
+import model.Board;
 import view.ClientView;
 
 public class Client extends Thread {
@@ -28,20 +31,82 @@ public class Client extends Thread {
 	
 	private void connectionSetup() throws IOException {
 		out.println("CONNECT " + name);
+		out.flush();
 		String response = in.readLine();
 		if (response.startsWith("ACCEPT_CONNECT")) {
 			ClientView.connected();
 		}
 	}
 	
-	private void getLobby() {
+	private String getLobby() {
 		out.println("REQUEST_LOBBY");
+		out.flush();
+		String res = null;
 		try {
-			String lobby = in.readLine();
-			ClientView.printLobby(lobby);
+			res = in.readLine();
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	private List<String> getClients(String lobby) {
+		List<String> res = new ArrayList<String>();
+		Scanner scan = new Scanner(lobby);
+		scan.skip("LOBBY");
+		while (scan.hasNext()) {
+			res.add(scan.next());
+		}
+		scan.close();
+		return res;
+	}
+	
+	private boolean suitableInvite(String invite) {
+		Scanner scan = new Scanner(invite);
+		scan.skip("INVITE");
+		int intwidth = 0;
+		int intheight = 0;
+		short shortwidth = 0;
+		short shortheight = 0;
+		if (scan.hasNextInt()) {
+			intwidth = Integer.parseInt(scan.next());
+			intheight = Integer.parseInt(scan.next());
+		}
+		else if (scan.hasNextShort()) {
+			shortwidth = scan.nextShort();
+			shortheight = scan.nextShort();
+		}
+		scan.close();
+		return (intwidth == Board.WIDTH && intheight == Board.HEIGHT) || (shortwidth == Board.WIDTH && shortheight == Board.HEIGHT); 
+	}
+	
+	private void getInvite() {
+		String invite = null;
+		while (true) {
+			try {
+				invite = in.readLine();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+			}
+			if (suitableInvite(invite)) {
+				break;
+			}
+		}
+		out.println("ACCEPT_INVITE");
+		out.flush(); 
+	}
+	
+	private void startGame() {
+		String lobby = getLobby();
+		ClientView.printLobby(lobby);
+		List<String> clients = getClients(lobby);
+		if (clients.size() == 1) {
+			getInvite();
+		}
+		if (clients.size() >= 2) {
+			
 		}
 	}
 	
@@ -51,7 +116,7 @@ public class Client extends Thread {
     		sock = new Socket(InetAddress.getByName(ClientView.getIP()), 4321);
     		communicationSetup();
     		connectionSetup();
-    		getLobby();
+    		startGame();
     		while (true) {
     			
     		}
---
+++ src/controller/InvitePeer.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * InvitePeer for recieving invites from the server.
+ * 
+ * @author  Jan-Jaap van Raffe and Wouter Bos
+ * @version v1.0
+ */
+public class InvitePeer implements Runnable {
+
+    protected BufferedReader in;
+
+    /**
+     * Constructor. creates a peer object based on the given parameters.
+     * @param input
+     * input from the socket
+     */
+    public InvitePeer(Socket sock) throws IOException {
+    	in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+    }
+
+    /**
+     * Reads strings of the stream of the socket-connection and writes the characters to the default output
+     */
+    public void run() {
+    	String bericht = null;
+    	while (true) {
+    		try {
+    			bericht = in.readLine();
+    			if (bericht.startsWith("GAME_START")) {
+    				break;
+    			}
+    			if (bericht.startsWith("ACCEPT_INVITE")) {
+    				break;
+    			}
+    		} catch (IOException e) {
+    			break;
+    		}
+    		Scanner scan = new Scanner(bericht);
+        	String command = scan.next();
+        	switch (command) {
+        	case "GAME_START":
+        	case "ACCEPT_INVITE": 	break;
+        	}
+    	}
+    }
+}
