73ebd868104d04b59c7a5ae3d3df911402f6a9c3
Wouter Bos <wouter_bos96@hotmail.com>
2015-01-21 17:15:24 +0100

    Capital letter

--- src/tests/naiveStrategyTest.java
+++ src/tests/naiveStrategyTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class naiveStrategyTest {
+public class NaiveStrategyTest {
 
 	@Before
 	public void setUp() throws Exception {
